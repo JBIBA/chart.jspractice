202cb01d95a0e99fcf9d9a6de757aa240f983f43
johanh0927 <59871531+johanh0927@users.noreply.github.com>
2020-09-22 17:11:48 +0200

    Update script.js

--- bubble-chart/script.js
+++ bubble-chart/script.js
@@ -1,26 +1,26 @@
 $(document).ready(function() {
 
-  var TITLE = 'Income, Test Scores, and Enrollment in Select Connecticut School Districts, 2009-13';
+  var TITLE = 'Surgar and Fat daily grams in selected nations in 2015';
 
-  var POINT_X = 'income'; // column name for x values in data.csv
-  var POINT_X_PREFIX = '$'; // prefix for x values, eg '$'
-  var POINT_X_POSTFIX = ''; // postfix for x values, eg '%'
+  var POINT_X = 'sugar'; // column name for x values in data.csv
+  var POINT_X_PREFIX = ''; // prefix for x values, eg 'gram'
+  var POINT_X_POSTFIX = ''; // postfix for x values, eg ' g'
 
-  var POINT_Y = 'grades'; // column name for y values in data.csv
-  var POINT_Y_PREFIX = ''; // prefix for x values, eg 'USD '
-  var POINT_Y_POSTFIX = ''; // postfix for x values, eg ' kg'
+  var POINT_Y = 'fat'; // column name for y values in data.csv
+  var POINT_Y_PREFIX = ''; // prefix for x values, eg 'gram'
+  var POINT_Y_POSTFIX = ''; // postfix for x values, eg ' g'
 
-  var POINT_R = 'enrollment'; // column name for radius in data.csv
-  var POINT_R_DESCRIPTION = 'Enrollment'; // description of radius value
+  var POINT_R = 'population'; // column name for radius in data.csv
+  var POINT_R_DESCRIPTION = 'population'; // description of radius value
   var POINT_R_PREFIX = ''; // prefix for radius values, eg 'USD '
-  var POINT_R_POSTFIX = ' students'; // postfix for radius values, eg ' kg'
+  var POINT_R_POSTFIX = ' population'; // postfix for radius values, eg ' kg'
   var R_DENOMINATOR = 800;  // use this to scale the dot sizes, or set to 1 if your dataset contains precise radius values
 
-  var POINT_NAME = 'district'; // point names that appear in tooltip
+  var POINT_NAME = 'nation'; // point names that appear in tooltip
   var POINT_COLOR = 'rgba(0,0,255,0.7)'; // point color, eg `black` or `rgba(10, 100, 44, 0.8)`
 
-  var X_AXIS = 'Median Household Income, USD';  // x-axis label and label in tooltip
-  var Y_AXIS = 'Grade, Relative to Average'; // y-axis label and label in tooltip
+  var X_AXIS = 'sugar (daily grams per capita, 2015)';  // x-axis label and label in tooltip
+  var Y_AXIS = 'fat (daily grams per capita, 2015)'; // y-axis label and label in tooltip
 
   var SHOW_GRID = true; // `true` to show the grid, `false` to hide
 
